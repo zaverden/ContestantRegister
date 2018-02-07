b59ce5c0d2f9dde6e6ee376456ac5338a70e7f57
DenisT <yadeniskotcvet@gmail.com>
2018-02-08 01:06:25 +0700

    Платная либа,с которой работает отправка email #OLIMP-16

--- ContestantRegister/Services/Email/EmailJob.cs
+++ ContestantRegister/Services/Email/EmailJob.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
-using System.Threading.Tasks;
 using ContestantRegister.Data;
+using EASendMail;
 using FluentScheduler;
 
 namespace ContestantRegister.Services.Email
@@ -24,16 +23,17 @@ namespace ContestantRegister.Services.Email
             {
                 var emails = _context.Emails.Where(e => !e.IsSended);
 
-                foreach (var email in emails)
+                foreach (var e in emails)
                 {
+                    /*
                     SmtpClient client = new SmtpClient();
                     client.UseDefaultCredentials = false;
-                    client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    client.DeliveryMethod = SmtpDeliveryMethod.PickupDirectoryFromIis;
                     client.EnableSsl = true;
 
                     client.Credentials = new NetworkCredential("acm@sfu-kras.ru", "67ChFylD");
                     client.Host = "mail.sfu-kras.ru";
-                    client.Port = 587;
+                    client.Port = 465;
 
                     //client.Credentials = new NetworkCredential("isit.open@yandex.ru", "contest");
                     //client.Host = "smtp.yandex.ru";
@@ -42,21 +42,47 @@ namespace ContestantRegister.Services.Email
                     MailMessage mailMessage = new MailMessage();
                     mailMessage.From = new MailAddress("acm@sfu-kras.ru", "Жюри");
                     //mailMessage.From = new MailAddress("isit.open@yandex.ru", "Жюри");
-                    mailMessage.To.Add(email.Address);
-                    mailMessage.Body = email.Message;
+                    mailMessage.To.Add(e.Address);
+                    mailMessage.Body = e.Message;
                     mailMessage.IsBodyHtml = false;
-                    mailMessage.Subject = email.Subject;
+                    mailMessage.Subject = e.Subject;
 
                     try
                     {
                         client.Send(mailMessage);
-                        email.IsSended = true;
+                        e.IsSended = true;
                     }
-                    catch (Exception e)
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(e);
+                    }*/
+
+                    SmtpServer server = new SmtpServer("mail.sfu-kras.ru", 465);
+
+                    server.User = "acm@sfu-kras.ru";
+                    server.Password = "67ChFylD";
+                    server.ConnectType = SmtpConnectType.ConnectSSLAuto;
+                    server.AuthType = SmtpAuthType.AuthPlain;
+
+                    SmtpMail mail = new SmtpMail("TryIt");
+                    mail.Subject = e.Subject;
+                    mail.TextBody = e.Message;
+                    mail.From = new EASendMail.MailAddress("Жюри", "acm@sfu-kras.ru");
+                    mail.To.Add(new EASendMail.MailAddress(e.Address));
+
+                    EASendMail.SmtpClient client = new EASendMail.SmtpClient();
+
+                    try
+                    {
+                        client.SendMail(server, mail);
+                        e.IsSended = true;
+                    }
+                    catch (Exception exception)
                     {
                         Console.WriteLine(e);
                     }
                     
+
                 }
 
                 ctx.SaveChangesAsync().Wait();
