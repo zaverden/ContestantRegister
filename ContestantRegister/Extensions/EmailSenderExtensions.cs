71023176fd253bb46c7e13f8f6e446c86bf3db5e
Denis Zavershinskiy <zaverden@gmail.com>
2018-02-11 22:09:45 +0700

    remove unused usings

--- ContestantRegister/Extensions/EmailSenderExtensions.cs
+++ ContestantRegister/Extensions/EmailSenderExtensions.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
-using ContestantRegister.Services;
 
 namespace ContestantRegister.Services
 {
--- ContestantRegister/ViewModels/ErrorViewModel.cs
+++ ContestantRegister/ViewModels/ErrorViewModel.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace ContestantRegister.Models
 {
     public class ErrorViewModel
