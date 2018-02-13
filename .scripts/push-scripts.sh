7a8b8950c3e4bf915f3354cafa938e2cf0e92702
Denis Zavershinskiy <zaverden@gmail.com>
2018-02-13 12:25:01 +0700

    move scripts

--- .scripts/push-scripts.sh
+++ .scripts/push-scripts.sh
@@ -4,5 +4,7 @@ echo list scripts
 ls ./.scripts/hosting/
 export SSHPASS=$HOST_PASSWORD
 echo start copy
-sshpass -e scp -o stricthostkeychecking=no ./.scripts/hosting/* $HOST_USERNAME@$HOST_ADDRESS:
+sshpass -e scp -o stricthostkeychecking=no -r ./.scripts/hosting/ $HOST_USERNAME@$HOST_ADDRESS:
+sshpass -e ssh -o stricthostkeychecking=no $HOST_USERNAME@$HOST_ADDRESS "chmod +x ./hosting/*.sh";
+
 echo done copy
