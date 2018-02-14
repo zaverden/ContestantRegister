47e92204f5b908d48e729586f9d7837afec099a3
DenisT <yadeniskotcvet@gmail.com>
2018-02-14 22:37:10 +0700

    Отключил жесткую валидацию пароля при регистрации  #OLIMP-22 Fixed

--- ContestantRegister/Startup.cs
+++ ContestantRegister/Startup.cs
@@ -48,6 +48,14 @@ namespace ContestantRegister
         {
             services.AddOptions();
             services.Configure<MailOptions>(Configuration.GetSection("SendEmail"));
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
