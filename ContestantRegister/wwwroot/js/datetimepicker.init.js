50802b37222b1a9ea5ed462b13ae0eb6815569fd
Denis Zavershinskiy <zaverden@gmail.com>
2018-03-18 22:58:53 +0700

    datetime picker #OLIMP-35 Fixed

---
+++ ContestantRegister/wwwroot/js/datetimepicker.init.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(() => {
+    moment.locale($('html').attr('lang'));
+    $('.apply-datetimepicker').datetimepicker();
+    $('.apply-datetimepicker-date').datetimepicker({ format: 'L' });
+    $('.apply-datetimepicker-time').datetimepicker({ format: 'LT' });
+});
+
