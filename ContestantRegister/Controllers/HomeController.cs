ef22315f64141f5d7320a055f584b0f46904430a
DenisT <yadeniskotcvet@gmail.com>
2018-04-26 15:04:41 +0700

    Экспорт в excel

--- ContestantRegister/Controllers/HomeController.cs
+++ ContestantRegister/Controllers/HomeController.cs
@@ -380,23 +380,27 @@ namespace ContestantRegister.Controllers
             
             var worksheet = package.Workbook.Worksheets.Add("Participants");
             worksheet.Cells["A1"].Value = "Email";
-            worksheet.Cells["B1"].Value = "Name";
-            worksheet.Cells["C1"].Value = "Surname";
+            worksheet.Cells["B1"].Value = "Surname";
+            worksheet.Cells["C1"].Value = "Name";
             worksheet.Cells["D1"].Value = "Patronymic";
             worksheet.Cells["E1"].Value = "TrainerEmail";
-            worksheet.Cells["F1"].Value = "TrainerName";
-            worksheet.Cells["G1"].Value = "ManagerEmail";
-            worksheet.Cells["H1"].Value = "ManagerName";
-            worksheet.Cells["I1"].Value = "Region";
-            worksheet.Cells["J1"].Value = "City";
-            worksheet.Cells["K1"].Value = "StudyPlace";
-            worksheet.Cells["L1"].Value = "Status";
-            worksheet.Cells["M1"].Value = "YaContestLogin";
-            worksheet.Cells["N1"].Value = "YaContestPassword";
-            worksheet.Cells["O1"].Value = "Area";
-            worksheet.Cells["P1"].Value = "Number";
-            worksheet.Cells["Q1"].Value = "ComputerName";
-            worksheet.Cells["R1"].Value = "ProgrammingLanguage";
+            worksheet.Cells["F1"].Value = "TrainerSurname";
+            worksheet.Cells["G1"].Value = "TrainerName";
+            worksheet.Cells["H1"].Value = "TrainerPatronymic";
+            worksheet.Cells["I1"].Value = "ManagerEmail";
+            worksheet.Cells["J1"].Value = "ManagerSurname";
+            worksheet.Cells["K1"].Value = "ManagerName";
+            worksheet.Cells["L1"].Value = "ManagerPatronymic";
+            worksheet.Cells["M1"].Value = "Region";
+            worksheet.Cells["N1"].Value = "City";
+            worksheet.Cells["O1"].Value = "StudyPlace";
+            worksheet.Cells["P1"].Value = "Status";
+            worksheet.Cells["Q1"].Value = "YaContestLogin";
+            worksheet.Cells["R1"].Value = "YaContestPassword";
+            worksheet.Cells["S1"].Value = "Area";
+            worksheet.Cells["T1"].Value = "Number";
+            worksheet.Cells["U1"].Value = "ComputerName";
+            worksheet.Cells["V1"].Value = "ProgrammingLanguage";
 
             int row = 1;
             foreach (var registration in registrations)
@@ -404,28 +408,32 @@ namespace ContestantRegister.Controllers
                 row++;
 
                 worksheet.Cells[row, 1].Value = registration.Participant1.Email;
-                worksheet.Cells[row, 2].Value = registration.Participant1.Name;
-                worksheet.Cells[row, 3].Value = registration.Participant1.Surname;
+                worksheet.Cells[row, 2].Value = registration.Participant1.Surname;
+                worksheet.Cells[row, 3].Value = registration.Participant1.Name;
                 worksheet.Cells[row, 4].Value = registration.Participant1.Patronymic;
                 worksheet.Cells[row, 5].Value = registration.Trainer.Email;
-                worksheet.Cells[row, 6].Value = $"{registration.Trainer.Surname} {registration.Trainer.Name} {registration.Trainer.Patronymic}";
-                    
+                worksheet.Cells[row, 6].Value = registration.Trainer.Surname;
+                worksheet.Cells[row, 7].Value = registration.Trainer.Name;
+                worksheet.Cells[row, 8].Value = registration.Trainer.Patronymic;
+
                 if (registration.Manager != null)
                 {
-                    worksheet.Cells[row, 7].Value = registration.Manager.Email;
-                    worksheet.Cells[row, 8].Value = $"{registration.Manager.Surname} {registration.Manager.Name} {registration.Manager.Patronymic}";
+                    worksheet.Cells[row, 9].Value = registration.Manager.Email;
+                    worksheet.Cells[row, 10].Value = registration.Manager.Surname;
+                    worksheet.Cells[row, 11].Value = registration.Manager.Name;
+                    worksheet.Cells[row, 12].Value = registration.Manager.Patronymic;
                 }
 
-                worksheet.Cells[row, 9].Value = registration.StudyPlace.City.Region.Name;
-                worksheet.Cells[row, 10].Value = registration.StudyPlace.City.Name;
-                worksheet.Cells[row, 11].Value = registration.StudyPlace.ShortName;
-                worksheet.Cells[row, 12].Value = registration.Status;
-                worksheet.Cells[row, 13].Value = registration.YaContestLogin;
-                worksheet.Cells[row, 14].Value = registration.YaContestPassword;
-                worksheet.Cells[row, 15].Value = registration.Area;
-                worksheet.Cells[row, 16].Value = registration.Number;
-                worksheet.Cells[row, 17].Value = registration.ComputerName;
-                worksheet.Cells[row, 18].Value = registration.ProgrammingLanguage;
+                worksheet.Cells[row, 13].Value = registration.StudyPlace.City.Region.Name;
+                worksheet.Cells[row, 14].Value = registration.StudyPlace.City.Name;
+                worksheet.Cells[row, 15].Value = registration.StudyPlace.ShortName;
+                worksheet.Cells[row, 16].Value = registration.Status;
+                worksheet.Cells[row, 17].Value = registration.YaContestLogin;
+                worksheet.Cells[row, 18].Value = registration.YaContestPassword;
+                worksheet.Cells[row, 19].Value = registration.Area;
+                worksheet.Cells[row, 20].Value = registration.Number;
+                worksheet.Cells[row, 21].Value = registration.ComputerName;
+                worksheet.Cells[row, 22].Value = registration.ProgrammingLanguage;
             }
 
             var ms = new MemoryStream();
